d017f88fdef45a9acc3130154270a7ca57fb4865
Anusha-Karanam <anushakaranambest@gmail.com>
2022-10-07 12:49:29 +0530

    commit27/9

---
+++ TE_CSharp/FileLines.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_CSharp
+{
+    internal class FileLines
+    {
+        public static void Main()
+        {
+            string path = "C:\\Users\\EI13153\\Desktop\\CSharp\\tables.txt";
+            string[] content;
+
+            //Read first line
+            content = File.ReadAllLines(path);
+
+            Console.WriteLine("First line of the file");
+            Console.WriteLine(content[0]);
+            Console.WriteLine("-------------------------------");
+
+            //Count number of lines in the file
+            int count = File.ReadAllLines(path).Length;
+            Console.WriteLine("Total number of lines in the file");
+            Console.WriteLine(count);
+            Console.WriteLine("-------------------------------");
+
+        }
+
+    }
+
+}
---
+++ TE_CSharp/FileOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_CSharp
+{
+    internal class FileOperation
+    {
+        public static void Main()
+        {
+            //file path
+            string path = "\\tables.txt";
+            string movepath = "C:\\Users\\EI13153\\Desktop\\CSharp\\tables.txt";
+           // string copypath = "C:\\Users\\EI13153\\Desktop\\CSharp\\tables-copy.txt";
+
+            //check whether the file exists
+            if (File.Exists(path))
+            {
+                //Copy file
+              // File.Copy(path, copypath);
+
+                //Move the file
+                //File.Move(copypath, movepath);
+
+                //Delete file
+                File.Delete(movepath);
+            }
+            else
+            {
+                Console.WriteLine("File does not Exist");
+            }
+        }
+    }
+}
